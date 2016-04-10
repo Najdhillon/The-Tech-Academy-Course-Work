b8258ef6e0f550ff08acd089e2c29c7eabacab96
Naj <najdhil@yahoo.com>
2016-04-10 16:02:21 -0700

    Fixing arrangement
    Reorganizing files that were out of place

---
+++ JavaScript/Practice/javascriptpractice.js
@@ -0,0 +1,64 @@
+var i = "hello world"
+
+var multiply = 4*4;
+
+multiply = 6*6;
+
+console.log(prompt("Where are we right now ?"));
+
+alert()
+
+var theNumber = Number(prompt("Pick a number",""))
+if (! isNaN(theNumber))
+alert("Your number is the square root" + theNumber + theNumber)
+
+var theNumber = Number(prompt("Pick a number",""));
+if (! isNaN(theNumber));
+alert("Your number is the square root" + theNumber + theNumber);
+else alert("Why don't you give me a number?");
+
+
+var sayHi = prompt("Hello what is your name");
+if (! isNaN(sayHi));
+alert("Why Hello there" + sayHi);
+else alert ("Hello Mr or Mrs Computer")
+
+
+/* EA Fifa 16 calculators */
+
+//Display cheapest prize
+
+var marketValue = prompt("What is the current market value for this card?");
+
+subtractionValue = marketValue * .05;
+
+cheapestPrice = marketValue - subtractionValue;
+
+alert("Your cheaptest value is at least is" + cheapsetPrice);
+
+//After testing in the console log, alert is not working for some reaosn
+
+var marketValue = prompt("What is the current market value for this card?");
+
+subtractionValue = marketValue * .05;
+
+cheapestPrice = marketValue - subtractionValue; 
+
+console.log("Your cheapest value to get your money back" + cheapestPrice);
+
+// could be possible to write a program where it 
+//increments as it gets to 7, but it sopts
+
+//write a loop that makes seven calls to console.log to output the following triangle. The hint involves using .length
+// it mentions to make seven calls, is saying are we creating seven variables
+// or are we just simply
+
+
+var first = "t";
+var second = "to";
+var third = "too";
+var fourth = "tooo";
+var fifth = "toooo";
+var sixth = "tooooo";
+var seventh = "toooooo";
+
--- The-Tech-Academy-Course-Work/Team-Projects/README.md
+++
@@ -1,8 +0,0 @@
-### Team Projects
-***
-
-Team projects that I worked on while at The Tech Academy.
-
-If you would like to see only my own solutions to software challenges, please check out my GitHub portfolio of my work with readme files detailing how I solved each one.
-
-Return to course work [main page](../../../)
