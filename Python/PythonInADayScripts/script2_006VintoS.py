3c259528a0c24d1145f64e7c2bd40fdacf12a3c5
Naj <najdhil@yahoo.com>
2016-05-09 23:25:35 -0700

    Adding in script2_006 file

---
+++ Python/PythonInADayScripts/script2_006VintoS.py
@@ -0,0 +1,9 @@
+#Cool stuff: Inserting variables into strings
+
+# .format() function inserts variables into strings by looking for {} in the string and replacing it with a value.
+# Here are some examples:
+
+print 'Epic number: {}!'.format(2)
+print 'Also strings: {}!'.format("Hello")
+print 'First: {}, Second: {}'.format(1,2)
+
