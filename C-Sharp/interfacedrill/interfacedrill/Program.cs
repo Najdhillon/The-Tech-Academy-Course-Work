02e26f720e44a6fc43f6c5dcb58cc383ca57d709
Naj <najdhil@yahoo.com>
2016-08-31 20:20:18 -0700

    More c# drills

---
+++ C-Sharp/interfacedrill/interfacedrill/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace interfacedrill
+{
+	interface Shape
+	{
+		void area();
+	}
+
+	class Rectangle : Shape
+	{
+		void Shape.area()
+		{
+			
+		}
+	}
+	 
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Shape ob = new Rectangle();
+
+			ob.area();
+		}
+	}
+}
